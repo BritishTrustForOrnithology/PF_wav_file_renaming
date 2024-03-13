1e2f92ab208e76a515db2da47844af651b0e5e2c
Simon Gillings <simon.gillings@bto.org>
2024-03-13 19:47:12 +0000

    add options to control how many folders and whether XMLs should be renamed

--- 02 perform the renaming.R
+++ 02 perform the renaming.R
@@ -4,38 +4,45 @@
 #' 
 #' Purpose:
 #'    Use the naming info produced by '01 collate naming info for batlogger files.R'
-#'    to update the names of wav files and xml files in selected folders. Also if 
-#'    a folder of Acoustic Pipeline results csv files is found, to update the file
-#'    names listed in those files to the corrected files also.
+#'    to update the names of wav files (and optionally xml files) in selected folders. 
+#'    Also if a folder of Acoustic Pipeline results csv files is found, to update 
+#'    the file names listed in those files to the corrected files also.
 #'    
 #' Usage:
 #'    1. To run the whole script in one go, press Ctrl+Shift+Enter
 #'    2. When prompted, select the csv file containing the naming info from the previous step.
-#'    3. When prompted, select the folder containing the audio files where present in individual site/date folders
-#'    4. When prompted, select the folder containing the audio files that has been merged into site folders
-#'    5. When prompted, select the folder containing the "DONE" audio files
-#'    6. Wait...this step will be slow as it will be scanning thousands of files. Updates will be shown on screen. 
+#'    3. When prompted, specify how many main folders containing audio you want to process (1, 2 or 3)
+#'    4. When prompted, specify if you want to rename XML files (WAV files are done by default)
+#'    5. Wait...this step will be slow as it will be scanning thousands of files. Updates will be shown on screen. 
 #'       When complete it will save various log files in the top level of the external hard drive 
 
-#' 
 library(rstudioapi)
 
-source('E:/functions and defaults.R')
+source('functions and defaults.R')
 
 #read the naming info
 file_names <- rstudioapi::selectFile(caption = 'Select the csv file containing the good and bad names...')
 names <- read.csv(file_names)
 
+#ask how many paths user wants to process
+n_paths <- as.numeric(rstudioapi::showPrompt(title = 'How many folders', message = 'Enter the number of folders you want to process, (e.g. 1, 2 or 3):'))
+if(!is.numeric(n_paths) | !n_paths %in% 1:3) stop('Number of folders needs to be a number between 1 and 3')
+
 #select the folders of audio
-path_renamed_unmerged <- rstudioapi::selectDirectory(path = dirname(file_names), caption='Select the folder containing renamed audio in folder...')
-path_renamed_merged <- rstudioapi::selectDirectory(path = dirname(file_names), caption='Select the folder containing renamed audio merged...')
-path_renamed_legacy <- rstudioapi::selectDirectory(path = dirname(file_names), caption='Select the folder containing renamed audio already processed pre Mara...')
-paths <- c(path_renamed_unmerged, path_renamed_merged, path_renamed_legacy)
-cat('About to process the following folders:\n  ', path_renamed_unmerged,'\n  ', path_renamed_merged,'\n  ', path_renamed_legacy,'\n')
+paths <- c(rep(NA,n_paths))
+for(p in 1:n_paths) {
+  paths[p] <- rstudioapi::selectDirectory(path = dirname(file_names), caption=paste0('Select a folder containing audio to be processed (',p,'/',n_paths,')...'))
+}
+cat('About to process the following folders:\n')
+for(p in paths) cat(p,'\n')
 
+#should XMLs be renamed?
+rename_xmls <- rstudioapi::showQuestion(title = 'Rename XMLs?', message = 'Should XML files be renamed?', ok = 'Yes', cancel = 'No')
 
+#iterate over all folders
 for(p in 1:length(paths)) {
   cat('Folder:',paths[p],'\n')
+
   wavs_bad_names <- list.files(paths[p], pattern = "*.wav", recursive = TRUE, full.names = TRUE)
   xmls_bad_names <- list.files(paths[p], pattern = "*.xml", recursive = TRUE, full.names = TRUE)
   cat('N wav files:', length(wavs_bad_names),'\n')  
@@ -60,18 +67,21 @@ for(p in 1:length(paths)) {
   write.csv(rename_results_wav, outfile_wav, row.names = FALSE)
   
   
-  cat("  FIXING XMLS...\n")
-  #for each original xml file, try to rename it
-  rename_results_xml <- list()
-  for(x in 1:length(xmls_bad_names)) {
-    this_xml <- xmls_bad_names[x]
-    rename_results_xml[x] <- rename_a_xml_file(this_xml)
+  #rename the xmls?
+  if(rename_xmls==TRUE) {
+    cat("  FIXING XMLS...\n")
+    #for each original xml file, try to rename it
+    rename_results_xml <- list()
+    for(x in 1:length(xmls_bad_names)) {
+      this_xml <- xmls_bad_names[x]
+      rename_results_xml[x] <- rename_a_xml_file(this_xml)
+    }
+    #unpack and report
+    rename_results_xml <- data.frame(original_name = xmls_bad_names, outcome = unlist(rename_results_xml))
+    table(rename_results_xml$outcome)
+    outfile_xml <- file.path(dirname(paths[p]), paste0(format(Sys.Date(), format = "%Y%m%d"), "_rename_batlogger_xml_files_",basename(paths[p]),".csv"))  
+    write.csv(rename_results_xml, outfile_xml, row.names = FALSE)
   }
-  #unpack and report
-  rename_results_xml <- data.frame(original_name = xmls_bad_names, outcome = unlist(rename_results_xml))
-  table(rename_results_xml$outcome)
-  outfile_xml <- file.path(dirname(paths[p]), paste0(format(Sys.Date(), format = "%Y%m%d"), "_rename_batlogger_xml_files_",basename(paths[p]),".csv"))  
-  write.csv(rename_results_xml, outfile_xml, row.names = FALSE)
   
   
   #fix the CSV results
